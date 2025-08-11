453487db0c7537388386c1fe96710c1d99a5fd7b
Joe Owens <joe.owens@answerdigital.com>
2025-08-11 19:41:54 +0100

    PROVIDERS: CaptchaAbstractionProvider

---
+++ ISL.Providers.Captcha.Abstractions/ICaptchaOperations.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace ISL.Providers.Captcha.Abstractions
+{
+    public interface ICaptchaOperations
+    {
+        /// <summary>
+        /// Uses Captcha service to validate a user request given a captcha token and user IP
+        /// </summary>
+        /// <returns>
+        /// A bool to signify whether the validation was successful
+        /// </returns>
+        ValueTask<bool> ValidateCaptchaAsync(string captchaToken, string userIp = "");
+    }
+}
---
+++ ISL.Providers.Captcha.Abstractions/Models/Exceptions/CaptchaProviderServiceException.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections;
+using Xeptions;
+
+namespace ISL.Providers.Captcha.Abstractions.Models.Exceptions
+{
+    public class CaptchaProviderServiceException : Xeption
+    {
+        public CaptchaProviderServiceException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+
+        public CaptchaProviderServiceException(string message, Xeption innerException, IDictionary data)
+            : base(message, innerException, data)
+        { }
+    }
+}
