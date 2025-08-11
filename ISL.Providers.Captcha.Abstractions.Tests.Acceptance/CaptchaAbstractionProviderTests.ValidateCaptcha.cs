9e0a4a82f28af7603a682e683fc6a19c14f56995
Joe Owens <joe.owens@answerdigital.com>
2025-08-11 20:13:38 +0100

    CaptchaAbstractionProvider Acceptance tests

---
+++ ISL.Providers.Captcha.Abstractions.Tests.Acceptance/CaptchaAbstractionProviderTests.ValidateCaptcha.cs
@@ -0,0 +1,39 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ISL.Providers.Captcha.Abstractions.Tests.Acceptance
+{
+    public partial class CaptchaAbstractionProviderTests
+    {
+        [Fact]
+        public async Task ShouldValidateCaptchaAsync()
+        {
+            // given
+            string randomCaptchaToken = GetRandomString();
+            string randomUserIp = GetRandomString();
+            string inputCaptchaToken = randomCaptchaToken.DeepClone();
+            string inputUserIp = randomUserIp.DeepClone();
+            bool randomOutputBool = GetRandomBool();
+            bool outputBool = randomOutputBool.DeepClone();
+            bool expectedPatient = outputBool.DeepClone();
+
+            this.captchaProviderMock.Setup(provider =>
+                provider.ValidateCaptchaAsync(inputCaptchaToken, inputUserIp))
+                    .ReturnsAsync(outputBool);
+
+            // when
+            bool actualPatient =
+                await this.captchaAbstractionProvider.ValidateCaptchaAsync(inputCaptchaToken, inputUserIp);
+
+            // then
+            actualPatient.Should().Be(expectedPatient);
+        }
+    }
+}
---
+++ ISL.Providers.Captcha.Abstractions.Tests.Acceptance/CaptchaAbstractionProviderTests.cs
@@ -0,0 +1,46 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace ISL.Providers.Captcha.Abstractions.Tests.Acceptance
+{
+    public partial class CaptchaAbstractionProviderTests
+    {
+        private readonly Mock<ICaptchaProvider> captchaProviderMock;
+        private readonly ICaptchaAbstractionProvider captchaAbstractionProvider;
+
+        public CaptchaAbstractionProviderTests()
+        {
+            captchaProviderMock = new Mock<ICaptchaProvider>();
+
+            this.captchaAbstractionProvider =
+                new CaptchaAbstractionProvider(captchaProviderMock.Object);
+        }
+
+        private static string GetRandomString()
+        {
+            int length = GetRandomNumber();
+
+            return new MnemonicString(wordCount: 1, wordMinLength: length, wordMaxLength: length).GetValue();
+        }
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static bool GetRandomBool()
+        {
+            int number = GetRandomNumber();
+
+            if (number > 5)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+    }
+}
