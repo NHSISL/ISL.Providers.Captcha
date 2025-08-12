e65d08b22df7d7389e42f0b1af0db2ee61def82d
Joe Owens <joe.owens@answerdigital.com>
2025-08-12 12:43:25 +0100

    FakeCaptcha acceptance test

---
+++ ISL.Providers.Captcha.FakeCaptcha.Tests.Acceptance/CaptchaServiceTests.ValidateCaptcha.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using Force.DeepCloner;
+using System.Threading.Tasks;
+
+namespace ISL.Providers.Captcha.FakeCaptcha.Tests.Acceptance
+{
+    public partial class CaptchaServiceTests
+    {
+        [Fact]
+        public async Task ShouldValidateCaptchaAsync()
+        {
+            // given
+            string randomCaptchaToken = "valid-captcha";
+            string randomUserIp = GetRandomString();
+            string inputCaptchaToken = randomCaptchaToken.DeepClone();
+            string inputUserIp = randomUserIp.DeepClone();
+            bool expectedResponse = true;
+
+            // when
+            bool actualResponse =
+                await this.fakeCaptchaProvider.ValidateCaptchaAsync(inputCaptchaToken, inputUserIp);
+
+            // then
+            actualResponse.Should().Be(expectedResponse);
+        }
+    }
+}
