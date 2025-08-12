bd80c33f0d5ca070bff584bfe077aa016507e27b
Joe Owens <joe.owens@answerdigital.com>
2025-08-12 11:30:24 +0100

    ShouldThrowValidationExceptionOnValidateCaptchaAsync -> FAIL

---
+++ ISL.Providers.Captcha.FakeCaptcha.Tests.Unit/Services/Foundations/Captcha/CaptchaServiceTests.ValidateCaptcha.Validations.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using ISL.Providers.Captcha.FakeCaptcha.Models.Foundations.Captcha.Exceptions;
+using System.Threading.Tasks;
+
+namespace ISL.Providers.Captcha.FakeCaptcha.Tests.Unit.Services.Foundations.Captcha
+{
+    public partial class CaptchaServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnValidateCaptchaAsync(string invalidCaptchaToken)
+        {
+            // given
+            var invalidArgumentCaptchaException =
+                new InvalidArgumentCaptchaException(
+                    "Invalid Captcha argument. Please correct the errors and try again.");
+
+            invalidArgumentCaptchaException.AddData(
+                key: "captchaToken",
+                values: "Text is invalid.");
+
+            var expectedCaptchaValidationException =
+                new CaptchaValidationException(
+                    message: "Captcha validation error occurred, please fix the errors and try again.",
+                    innerException: invalidArgumentCaptchaException);
+
+            // when
+            bool validateCaptchaAction =
+                captchaService.ValidateCaptcha(invalidCaptchaToken, invalidCaptchaToken);
+
+            CaptchaValidationException actualException =
+                Assert.Throws<CaptchaValidationException>(() => validateCaptchaAction);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedCaptchaValidationException);
+        }
+    }
+}
---
+++ ISL.Providers.Captcha.FakeCaptcha/Models/Foundations/Captcha/Exceptions/InvalidArgumentCaptchaException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using Xeptions;
+
+namespace ISL.Providers.Captcha.FakeCaptcha.Models.Foundations.Captcha.Exceptions
+{
+    public class InvalidArgumentCaptchaException : Xeption
+    {
+        public InvalidArgumentCaptchaException(string message)
+            : base(message)
+        { }
+    }
+}
